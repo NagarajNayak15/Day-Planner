fd92adb09d8601b0532b81b0d00caaf9fb997ecc
NagarajNayak15 <nagarajbn004@gmail.com>
2026-07-15 22:53:15 +0530

    origin added

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,7 +14,7 @@ export function createApp(): Application {
 
   app.use(helmet());
   app.use(
-    cors({"credentials":true})
+    cors({"origin":["https://planyourtimes.netlify.app/","http://localhost:5173"],"credentials":true})
   );
   app.use(express.json({ limit: '1mb' }));
   app.use(cookieParser());
